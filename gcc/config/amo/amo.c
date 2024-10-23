d41a6244dd37bf5baf301035a4940e8c0098c46e
yahong <yechan0815@naver.com>
2024-10-23 15:09:28 +0900

    branch

--- gcc/config/amo/amo.c
+++ gcc/config/amo/amo.c
@@ -1115,14 +1115,14 @@ amo_expand_compare_branch (rtx *operands)
 
 	gcc_assert(operands[3] != NULL);
 
-	if (!register_operand (opn1, SImode))
+	if (!register_operand (opn1, mode))
   {
-		opn1 = force_reg (SImode, opn1);
+		opn1 = force_reg (mode, opn1);
   }
 
-	if (!register_operand (opn2, SImode))
+	if (!register_operand (opn2, mode))
   {
-		opn2 = force_reg (SImode, opn2);
+		opn2 = force_reg (mode, opn2);
   }
 
 	code = gen_rtx_fmt_ee (GET_CODE (operands[0]), SImode, opn1, opn2);
