54b2e61426e471fa07f26c2d4527aeb51390cfca
yahong <yechan0815@naver.com>
2024-10-24 17:24:52 +0900

    int

--- gcc/config/amo/amo.h
+++ gcc/config/amo/amo.h
@@ -120,15 +120,12 @@ do                                              \
 #define DEFAULT_PCC_STRUCT_RETURN 0
 
 /* Register usage.  */
-
-/* First 32-bit register is R12.  */
-//#define AMO_FIRST_DWORD_REGISTER   12 // TODO
-
 #define FIRST_PSEUDO_REGISTER      32
 
 /* 1 for registers that have pervasive standard uses
    and are not available for the register allocator.
-   On the AMO, only the stack pointer (r29) is such.  */
+   On the AMO, the interrupt register and the
+  stack pointer (r29) is such.  */
 #define FIXED_REGISTERS                                                          \
   {                                                                              \
   /* r0 r1 r2 r3 r4 r5 r6 r7 */                                                  \
@@ -137,8 +134,8 @@ do                                              \
     0, 0, 0, 0, 0, 0, 0, 0,                                                      \
   /* r16 r17 r18 r19 r20 r21 r22 r23 */                                          \
     0, 0, 0, 0, 0, 0, 0, 0,                                                      \
-  /* r24 r25 r26 r27 r28 fp sp lr */                                             \
-    0, 0, 0, 0, 0, 0, 1, 0                                                       \
+  /* r24 r25 r26 r27(interrupt only) r28 fp sp lr */                                             \
+    0, 0, 0, 1, 0, 0, 1, 0                                                       \
   }
 
 /* 1 for registers not available across function calls.
